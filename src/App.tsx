21b3a64074d25b5c575a8c58de4887fd030fed9c
Yuriy Shchebetun <yuriyshchebetun@gmail.com>
2022-09-05 18:53:25 +0300

    RTK settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.scss';
+import Counter from './features/Counter/Counter';
 
 function App() {
     return (
         <div className="App">
             <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>It's going to be an awesome app ;)</p>
+                <Counter />
             </header>
         </div>
     );
